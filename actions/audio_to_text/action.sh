9050608de0a70dfa7e56f224daf4bf8777280b22
mavek87 <matver87@gmail.com>
2025-03-18 19:16:26 +0100

    refactoring removing duplication

--- actions/audio_to_text/action.sh
+++ actions/audio_to_text/action.sh
@@ -7,35 +7,17 @@ export CURRENT_SMART_ACTION_NAME="$script_folder_name"
 export SMART_ACTIONS_CONFIG_FOLDER="${SMART_ACTIONS_PROJECT_DIR}/actions/${CURRENT_SMART_ACTION_NAME}"
 export SMART_ACTIONS_CONFIG_FILE="${SMART_ACTIONS_CONFIG_FOLDER}/action.conf"
 
-# TODO: duplicated code except last lines
-read_command_action_builder_data_output() {
-  declare -A CMD_VARS
+source "${script_dir}/../commons.sh"
 
-  while IFS='=' read -r key value; do
-    key=$(echo "$key" | tr -d '[:space:]')
-    value=$(echo "$value" | tr -d '[:space:]')
+execute_action() {
+  echo "Execute: $CURRENT_SMART_ACTION_NAME"
 
-    [[ -z "$key" ]] && continue
-
-    CMD_VARS["$key"]="${value:-""}"
-  done < <(tr -d '\r' <"$SMART_ACTIONS_COMMAND_BUILDER_OUTPUT_FILE")
-
-  rm -rf "$SMART_ACTIONS_COMMAND_BUILDER_OUTPUT_FILE" 2>/dev/null || true
-
-  input_file="${CMD_VARS["input_file"]}"
+  input_file="${CMD_ARGS["input_file"]}"
   if [ ! -f "$input_file" ]; then
     # TODO is it ok? No complete help print...
     echo -e "${SMART_ACTIONS_COLOR_RED}Error: input file '$input_file' does not exist${SMART_ACTIONS_COLOR_RESET}"
     exit 1
   fi
-
-  model="${CMD_VARS["model"]}"
-  task="${CMD_VARS["task"]}"
-  language="${CMD_VARS["language"]}"
-}
-
-execute_action() {
-  echo "Execute: $CURRENT_SMART_ACTION_NAME"
 
   faster_whisper_cmd="${FASTER_WHISPER_DIR}/faster-whisper --vad_method pyannote_v3 --device cuda --model ${model} --output_format text --task ${task}"
   if [[ -n "$language" ]]; then
@@ -45,7 +27,6 @@ execute_action() {
 
   cp "$input_file" "${FASTER_WHISPER_DIR}/rec_audio.wav" &&
     $faster_whisper_cmd &&
-    #    echo type "$(tr '\n' ' ' <"${SMART_ACTIONS_PROJECT_DIR}/rec_audio.text")" |
     # TODO: evaluate if typedelay and typehold should be dynamic values
     echo "typedelay 2
             typehold 2
@@ -60,6 +41,10 @@ if [[ $result -ne 0 ]]; then
   exit $result
 fi
 
-read_command_action_builder_data_output
+load_args_from_built_command
+
+model="${CMD_ARGS["model"]}"
+task="${CMD_ARGS["task"]}"
+language="${CMD_ARGS["language"]}"
 
 execute_action
